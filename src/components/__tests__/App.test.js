2b2c8de8424e5d42b292289b94b123f9168b8492
Nestor Tamares <jrtamares@gmail.com>
2019-01-27 23:03:00 -0600

    Run shallow render test
    Add Enzyme API. Test if components exist

---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import App from '../App';
+import CommentBox from '../CommentBox';
+
+it('shows a comment box', () =>{
+  const component = shallow(<App />);
+  
+  expect(component.find(CommentBox).length).toEqual(1);
+});
